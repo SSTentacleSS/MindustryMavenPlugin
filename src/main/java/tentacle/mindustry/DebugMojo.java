f1967024bca95b6633b132d02c312e3859c3f528
SSTentacleSS <ss_tentacle_ss@zohomail.eu>
2022-06-19 23:33:23 +0300

    Change goal name

--- src/main/java/tentacle/mindustry/DebugMojo.java
+++ src/main/java/tentacle/mindustry/DebugMojo.java
@@ -15,7 +15,7 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-@Mojo(name = "mindustry:debug", defaultPhase = LifecyclePhase.NONE)
+@Mojo(name = "debug", defaultPhase = LifecyclePhase.NONE)
 public class DebugMojo extends AbstractMojo {
     
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
