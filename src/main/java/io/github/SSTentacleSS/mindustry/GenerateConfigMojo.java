fa85c40fbee66c876bdff91be0f718a371b53a27
SSTentacleSS <ss_tentacle_ss@zohomail.eu>
2022-06-26 17:17:48 +0300

    Bug fixes

--- src/main/java/io/github/SSTentacleSS/mindustry/GenerateConfigMojo.java
+++ src/main/java/io/github/SSTentacleSS/mindustry/GenerateConfigMojo.java
@@ -3,7 +3,12 @@ package io.github.SSTentacleSS.mindustry;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Supplier;
 import java.util.jar.JarFile;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
@@ -32,19 +37,43 @@ public class GenerateConfigMojo extends AbstractMojo {
                 )
             );
 
-            thisFile.stream()
+            List<String> files = thisFile.stream()
                 .filter(fileOrDirEntry -> fileOrDirEntry.getName().startsWith("editors") && !fileOrDirEntry.isDirectory())
-                .forEach(fileEntry -> {
+                .map(fileEntry -> fileEntry.toString())
+                .toList();
+            Supplier<Stream<String>> filesStreamSupplier = () -> files.stream();
+
+            if (
+                !filesStreamSupplier
+                    .get()
+                    .anyMatch(file -> file.startsWith("editors/." + editor))
+            ) throw new MojoExecutionException(editor + " editor is not supported. Supported editors: " +
+                filesStreamSupplier.get().collect(
+                    StringBuilder::new,
+                    (x, y) -> {
+                        Matcher matcher = Pattern
+                            .compile("^editors\\/\\.([^\\/]*)[\\d\\D]+", 0)
+                            .matcher(y);
+                        matcher.find();
+
+                        if (x.indexOf(matcher.group(1)) == -1)
+                            x.append("\"" + matcher.group(1) + "\" ");
+                    }, StringBuilder::append
+                ).toString()
+            );
+
+            filesStreamSupplier.get()
+                .forEach(file -> {
                     try {
                         FileUtils.writeStringToFile(
                             Path.of(
-                                fileEntry.getName().replace("editors/", "")
+                                file.replace("editors/", "")
                             ).toFile(),
-                            IOUtils.toString(getClass().getResourceAsStream("/" + fileEntry.getName()), Charset.defaultCharset()),
+                            IOUtils.toString(getClass().getResourceAsStream("/" + file), Charset.defaultCharset()),
                             Charset.defaultCharset()
                         );
                     } catch (IOException e) {
-                        getLog().error("Error writing " + fileEntry.getName(), e);
+                        getLog().error("Error writing " + file, e);
                     }
                 });
         } catch (IOException e) {
